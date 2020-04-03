0dcbdcf86e2eefc79255e0379bd4bca6827dcb83
dhileepbalaji <33687091+dhileepbalaji@users.noreply.github.com>
2020-04-04 01:32:56 +0530

    Update generator_template.py

--- utils/composegenerator/generator_template.py
+++ utils/composegenerator/generator_template.py
@@ -38,6 +38,12 @@ for service in finalComposeFiledata["services"].keys():
         # Adding Deploy Section settings
         finalComposeFiledata["services"][service]["deploy"] = tempateComposeFiledata["services"]["template"]["deploy"]
 
+        # Appending Labels
+        finalComposeFiledata["services"][service]["deploy"]["labels"].append("traefik.http.routers"+ service + "entrypoints=devhttp,devhttps")
+        finalComposeFiledata["services"][service]["deploy"]["labels"].append("traefik.http.service"+ service + "service=" + service)
+        finalComposeFiledata["services"][service]["deploy"]["labels"].append("traefik.http.rules"+ service + "$\{TRAEFIK_FRONTEND_HOST\}")
+        finalComposeFiledata["services"][service]["deploy"]["labels"].append("traefik.http.services"+ service + 'loadbalancer.server.port=$\{TRAEFIK_PORT\}')
+
         
 
     else:
