f9dcaec446e2f1d5780ed2c4a0067ed99e8c3ce4
dhileepbalaji <33687091+dhileepbalaji@users.noreply.github.com>
2020-04-05 16:49:18 +0530

    adding custom traefik router name

--- utils/composegenerator/generator_template.py
+++ utils/composegenerator/generator_template.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 import yaml
 import argparse
-import os,sys
+import os, sys
+
 # Create the parser
 arg_parser = argparse.ArgumentParser(description='Generate Compose file')
 # Add the arguments
 arg_parser.add_argument('--servicename', required=True, metavar='servicename', type=str,
                         help='enter the service name used in compose file to add customization')
+arg_parser.add_argument('--traefikservicename', required=False, metavar='traefikservicename', type=str,
+                        default="traefikservicename",
+                        help='enter the src composefileaname to add customization')
 arg_parser.add_argument('--composefileaname', required=False, metavar='composefileaname', type=str,
                         default="docker-compose-env.yml",
                         help='enter the src composefileaname to add customization')
@@ -14,37 +18,52 @@ arg_parser.add_argument('--composefileaname', required=False, metavar='composefi
 # Execute the parse_args() method
 args = arg_parser.parse_args()
 
-srcComposeFile = open(os.path.join(sys.path[0],args.composefileaname), "r+")
-tempateComposeFile = open(os.path.join(sys.path[0],"docker-compose-template.yml"), "r+")
+srcComposeFile = open(os.path.join(sys.path[0], args.composefileaname), "r+")
+tempateComposeFile = open(os.path.join(sys.path[0], "docker-compose-template.yml"), "r+")
 finalComposeFiledata = yaml.load(srcComposeFile, Loader=yaml.FullLoader)
 tempateComposeFiledata = yaml.load(tempateComposeFile, Loader=yaml.FullLoader)
 srcComposeFile.close()
 
 for service in finalComposeFiledata["services"].keys():
-    if service == args.servicename :
-    
+    if service == args.servicename:
+
         # changing version
         finalComposeFiledata["version"] = tempateComposeFiledata["version"]
         # Adding Networks
         finalComposeFiledata["networks"] = tempateComposeFiledata["networks"]
 
         # Adding Env file to Service
-        finalComposeFiledata["services"][service]["env_file"] = tempateComposeFiledata["services"]["template"]["env_file"]
-        
-        # Adding network to service
-        finalComposeFiledata["services"][service]["networks"] = tempateComposeFiledata["services"]["template"]["networks"]
+        finalComposeFiledata["services"][service]["env_file"] = tempateComposeFiledata["services"]["template"][
+            "env_file"]
 
-        
+        # Adding network to service
+        finalComposeFiledata["services"][service]["networks"] = tempateComposeFiledata["services"]["template"][
+            "networks"]
+
         # Adding Deploy Section settings
         finalComposeFiledata["services"][service]["deploy"] = tempateComposeFiledata["services"]["template"]["deploy"]
 
-        # Appending Labels
-        finalComposeFiledata["services"][service]["deploy"]["labels"].append("traefik.http.routers."+ service + ".entrypoints=${TRAEFIK_ENTRYPOINT}")
-        finalComposeFiledata["services"][service]["deploy"]["labels"].append("traefik.http.routers."+ service + ".service=" + service)
-        finalComposeFiledata["services"][service]["deploy"]["labels"].append("traefik.http.routers."+ service + '.rule=${TRAEFIK_FRONTEND_HOST}')
-        finalComposeFiledata["services"][service]["deploy"]["labels"].append("traefik.http.services."+ service + '.loadbalancer.server.port=${TRAEFIK_PORT}')
+        if traefikservicename:
+            # Appending Labels
+            finalComposeFiledata["services"][service]["deploy"]["labels"].append(
+                "traefik.http.routers." + traefikservicename + ".entrypoints=${TRAEFIK_ENTRYPOINT}")
+            finalComposeFiledata["services"][service]["deploy"]["labels"].append(
+                "traefik.http.routers." + traefikservicename + ".service=" + service)
+            finalComposeFiledata["services"][service]["deploy"]["labels"].append(
+                "traefik.http.routers." + traefikservicename + '.rule=${TRAEFIK_FRONTEND_HOST}')
+            finalComposeFiledata["services"][service]["deploy"]["labels"].append(
+                "traefik.http.services." + traefikservicename + '.loadbalancer.server.port=${TRAEFIK_PORT}')
 
-        
+        # Appending Labels
+        finalComposeFiledata["services"][service]["deploy"]["labels"].append(
+            "traefik.http.routers." + service + ".entrypoints=${TRAEFIK_ENTRYPOINT}")
+        finalComposeFiledata["services"][service]["deploy"]["labels"].append(
+            "traefik.http.routers." + service + ".service=" + service)
+        finalComposeFiledata["services"][service]["deploy"]["labels"].append(
+            "traefik.http.routers." + service + '.rule=${TRAEFIK_FRONTEND_HOST}')
+        finalComposeFiledata["services"][service]["deploy"]["labels"].append(
+            "traefik.http.services." + service + '.loadbalancer.server.port=${TRAEFIK_PORT}')
+
 
     else:
         # Adding network to service
@@ -58,5 +77,5 @@ yaml.SafeDumper.add_representer(
 )
 
 # Output_file
-output_file = open(os.path.join(sys.path[0],"docker-compose-env.yml"), "w+")
+output_file = open(os.path.join(sys.path[0], "docker-compose-env.yml"), "w+")
 yaml.safe_dump(finalComposeFiledata, output_file, default_flow_style=False)
